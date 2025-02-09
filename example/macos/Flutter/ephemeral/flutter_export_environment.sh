9f710aa5a59caae0ccc734b289d938b30d856545
Bansook Nam <short28kr@gmail.com>
2025-02-09 10:31:05 +0900

    add macos

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/pawankumar/fvm/versions/stable"
-export "FLUTTER_APPLICATION_PATH=/Users/pawankumar/Documents/dev/flutter/VelocityX/example"
+export "FLUTTER_ROOT=/Users/nam/fvm/versions/local"
+export "FLUTTER_APPLICATION_PATH=/Users/nam/Projects/flutter-lib/VelocityX/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/nam/Projects/flutter-lib/VelocityX/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/nam/Projects/flutter-lib/VelocityX/example/.dart_tool/package_config.json"
